be252e654d26bf545138bb08a2b384772005ba73
Bruno L. Carli <brunolcarli@gmail.com>
2023-06-11 04:29:36 -0300

    ADD prompt banner

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from src.commands import bot, check_inactive_channels
-from src.settings import TOKEN, ENV_REF
+from src.settings import TOKEN, ENV_REF, __version__
 from src.keep_alive import keep_alive
+
+
+banner = r'''
+_______________________________________
+  ____            __            _     
+ /_  / ___ ____  / /  __ ______(_)__ _
+  / /_/ _ `/ _ \/ _ \/ // / __/ / _ `/
+ /___/\_,_/ .__/_//_/\_, /_/ /_/\_,_/ 
+         /_/        /___/            
+_______________________________________
+'''
 
 if __name__ == '__main__':
     if ENV_REF == 'replit':
         keep_alive()
+
+    print(banner)
+    print(f'Version: {__version__}')
+
     bot.loop.create_task(check_inactive_channels())
     bot.ticket_categories = {}  # Dictionary to store ticket categories
     bot.run(TOKEN)
